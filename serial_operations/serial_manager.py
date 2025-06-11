587e627cfdabaf291bede5874f1504591700480a
Adam Carrera <carrera.adam0@gmail.com>
2025-06-11 16:57:10 -0500

    work on reading from digital multimeter

--- serial_operations/serial_manager.py
+++ serial_operations/serial_manager.py
@@ -1,51 +1,102 @@
 import serial
 import time
-from typing import Any
+from typing import Any, Self
 import timeit
+from enum import Enum
+
+
+class BaudRate(Enum):
+    """BaudRates used by BK Precision 5492C"""
+
+    BR_4800 = 4800
+    BR_9600 = 9600
+    BR_19200 = 19200
+    BR_38400 = 38400
+    BR_57600 = 57600
+    BR_115200 = 115200
+
+
+class Voltage(float):
+    """
+    class for handling output of digitla multimeter.
+    rounds given float value to one decimal when created
+    implements __str__ to keep # of digits consistent because floats
+        leave off trailing zeroes
+    """
+
+    precision = 1
+
+    def __new__(cls, value: float) -> "Voltage":
+        return super().__new__(cls, round(value, cls.precision))
+
+    def __str__(self) -> str:
+        return f"{self:.{self.precision}f}"
 
 
 class BKPrecision_5492C:
-    """docstring for BKPrecision_5492C."""
+    """serial library for BKPrecision_5492C."""
 
-    def __init__(self, port: str, timeout: float, baudrate: int) -> None:
-        self.serial = serial.Serial(port=port, timeout=timeout, baudrate=baudrate)
+    def __init__(self, port: str, timeout: float, baudrate: BaudRate) -> None:
+        self.serial = serial.Serial(port=port, timeout=timeout, baudrate=baudrate.value)
 
-    def get_voltage(self) -> float:
+    def __enter__(self) -> Self:
+        """set up device for measuring voltage"""
+        self._write_command("*RST")
+        self._write_command("SENS:VOLT:DC:NPLC 0.01")
+        self._write_command("TRIG:SOUR IMM")
+        self._write_command("TRIG:COUN 1")
+        self._write_command("SAMP:COUN 1")
+        self._write_command("INIT")
+        time.sleep(3)
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:  # type: ignore
+        """teardown when finished."""
+        self._write_command("*RST")
+        self._write_command("LOC")
+
+    def query_voltage(self) -> Voltage:
+        """return value from the stream of data & return zero if it doesn't exist"""
+        self._write_command("INIT")
         try:
-            return float(self.query("MEAS:VOLT:DC?"))
+            raw_data = float(self.query("FETCh?"))
+            return Voltage(raw_data)
         except ValueError:
-            return 0.0
+            return Voltage(0.0)
 
     def query(self, cmd: str) -> str:
         self._write_command(cmd)
-
-        return self._read_until_response()
+        return self.read_value()
 
     def _write_command(self, cmd: str) -> None:
         """write serial command to device with newline termination"""
         try:
             self.serial.write(cmd.encode() + b"\n")
-
         except serial.SerialException as e:
             print(e)
 
-    def _read_until_response(self, max_retries: int = 3, delay: float = 0.05) -> str:
-        for _ in range(max_retries):
-            response = self.serial.readline()
-
-            decoded = response.decode().strip()
-
-            if decoded and not decoded.startswith("MEAS:"):
-                return decoded
-
-            time.sleep(delay)
+    def read_value(self) -> str:
+        """
+        read all of the lines and return the last one which has the voltage.
+        if it's not there, then return an empty string
+        """
+        response = self.serial.read_until(b"").strip()
+        if response:
+            split = response.split(b"\r\n")
+            decoded = split[-1].decode()
+            return decoded
         return ""
 
 
 if __name__ == "__main__":
-    s = BKPrecision_5492C("com8", 0.5, 19200)
+    with BKPrecision_5492C("com8", 0.1, baudrate=BaudRate.BR_57600) as dmm:
 
-    duration = timeit.timeit(s.get_voltage, number=100)
-    print(f"Avg time per call: {duration / 100:.6f}s")
+        # Benchmark
+        start = time.perf_counter()
+        for _ in range(100):
+            voltage = dmm.query_voltage()
+            print(voltage)
+        end = time.perf_counter()
 
-    s._write_command("LOC")
+        print(f"Avg time per call: {(end - start)/100:.6f}s")
